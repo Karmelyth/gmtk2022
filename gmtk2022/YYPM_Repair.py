da3ddf95b7c4f0b57d97f35b6f0f1e51a798bc01
Unknown <karmelyht@gmail.com>
2022-07-15 21:30:45 +0200

    python script the squeaquel

--- gmtk2022/YYPM_Repair.py
+++ gmtk2022/YYPM_Repair.py
@@ -3,15 +3,19 @@ import string
 import pathlib
 
 def repair_filenames():
+    count = 0
     path = pathlib.Path(".") #assigns current directory as base
-    for folder in path.iterdir():
+    for folder in path.rglob("*"):
         if folder.is_dir():
-            for file in folder.iterdir():
+            tpath = pathlib.Path(folder.name)
+            for file in folder.rglob("*"):
                 if file.is_file():
-                    if file.name.lower() == folder.name.lower() + ".yy":
-                        old_name = file.name
-                        new_name = folder.name + ".yy"
-                        print("RENAMING ", old_name, "TO ", new_name)
-                        file.rename(path / folder.name / new_name)
+                    if file.name.lower() == folder.name.lower() + ".yy" and file.name != folder.name + ".yy":
+                        count += 1
+                        target = pathlib.Path(folder)
+                        print("RENAMING",file.name,"TO",folder.name + ".yy")
+                        file.rename(target / (folder.name + ".yy"))
+                        print("RENAMED FILE");
+    print(count,"FILES RENAMED!")
 if __name__ == "__main__":
     repair_filenames()
